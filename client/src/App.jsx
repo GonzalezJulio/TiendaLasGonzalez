e620a1995eda010f4a7c4be434ccfb123a9034b9
Julio Gonzalez <aresden113@gmail.com>
2024-08-13 19:14:30 -0300

    Second Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-
+import { BrowserRouter } from "react-router-dom";
+//minuto 27:09
 export default function App() {
-  return <h1 className='text-red-500'>App</h1>
+  return <BrowserRouter >
+  <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-out" element={<SignOut />} /> 
+    <Route path="/about" element={<About />} />
+    <Route path="/profile" element={<Profile />} />
+  </Routes>
+  </BrowserRouter>
 }

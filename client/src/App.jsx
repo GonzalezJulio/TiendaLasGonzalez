3f1b3f66c1a4ee01231cb04854ec570bd556ddec
aresden113 <aresden113@gmail.com>
2024-08-14 16:42:07 -0300

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import SignOut from "./pages/SignOut";
-import Profile from "./pages/Profile";
-
-
-
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import Profile from './pages/Profile';
+import About from './pages/About';
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/register" element={<SignOut />} />
-       
-        <Route element={<PrivateRouter />}>
-
-          <Route path="/create-product" element={<CreateProduct />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/carts" element={<Carts />} />
-          
-        </Route>
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-out" element={<SignOut />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/about" element={<About />} />
+        
+  </Routes>
+  </BrowserRouter>
 }
